cb869823c7d3619a3c600e23ba3cf27601b860a8
adcreech <54107541+adcreech@users.noreply.github.com>
2019-09-09 18:17:00 -0400

    Add files via upload

---
+++ ConvertFrom-Base64String.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.SYNOPSIS
+  Converts strings from base64 encoded strings into plaintext.
+.DESCRIPTION
+  The ConvertFrom-Base64String function returns a plaintext string for a base64 encoded string input.
+.PARAMETER Base64String
+  Specifies the base64 encoded string input to return in a decoded plaintext format.
+.INPUTS
+  None
+.OUTPUTS
+  System.String
+  The plaintext decoded string is output as a System.String.
+.NOTES
+  Version:        0.1
+  Author:         Adam Creech
+  Creation Date:  5/29/2018
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  PS C:\>ConvertFrom-Base64String 'VXNlcm5hbWU='
+
+  This command decodes the base64 encoded string "VXNlcm5hbWU=" (without quotes) into a plaintext string.
+#>
+function ConvertFrom-Base64String {
+param(
+    [Parameter(
+        Mandatory = $true
+    )]
+    [string]$Base64String
+)
+
+$decoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Base64String))
+Write-Output -InputObject $decoded
+}
---
+++ ConvertTo-Base64String.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.SYNOPSIS
+  Converts strings into base64 encoded strings.
+.DESCRIPTION
+  The ConvertTo-Base64String function returns a base64 encoded string for a string input.
+.PARAMETER PlainText
+  Specifies the string input to return in an encoded base64 format. Use single quotes to encapsulate strings containing spaces.
+.INPUTS
+  None
+.OUTPUTS
+  System.String
+  The base64 encoded string is output as a System.String.
+.NOTES
+  Version:        0.1
+  Author:         Adam Creech
+  Creation Date:  5/29/2018
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  PS C:\>ConvertTo-Base64String 'Username'
+
+  This command converts the UTF8 plaintext string "Username" (without quotes) into a base64 encoded string.
+#>
+function ConvertTo-Base64String {
+param(
+    [Parameter(
+        Mandatory = $true
+    )]
+    [string]$PlainText
+)
+
+$encoded = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($PlainText))
+Write-Output -InputObject $encoded
+}
