94943dd42379b3831e72fca7d726928370d11488
adcreech <54107541+adcreech@users.noreply.github.com>
2020-01-22 19:17:49 -0500

    Add files via upload

---
+++ ConvertFrom-UnixTime.ps1
@@ -0,0 +1,13 @@
+﻿Function ConvertFrom-UnixTime {  
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [Int32]$UnixTime
+    )
+    begin {
+        $startdate = Get-Date –Date '01/01/1970' 
+    }
+    process {
+        $timespan = New-Timespan -Seconds $UnixTime
+        $startdate + $timespan
+    }
+}
